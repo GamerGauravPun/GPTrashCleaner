b964f9a50fd6311d440de2776a8fe368d6391c91
GauravPun <gamer.gauravpun@gmail@gmail.com>
2019-04-17 00:32:22 +0530

    -_-!

--- GPTrashCleaner/Program.cs
+++ GPTrashCleaner/Program.cs
@@ -10,71 +10,239 @@ namespace GPTrashCleaner
 {
     class Program
     {
-        static float Percent;
+        static string perTempLoc;
+        static string tempLoc;
+        static string prefetchLoc;
 
-        static List<string> otherFiles;
+        static string[] perTempFils;
+        static string[] perTempFols;
+
+        static string[] tempFils;
+        static string[] tempFols;
+
+        static string[] preFils;
+        static string[] preFols;
 
         static void Main(string[] args)
         {
             Welcome(Environment.UserName);
-            otherFiles = new List<string>();
         }
 
         static void Welcome(string name) {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("  # Do you want to clean trash ? " + name + "!");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("    Type: ");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("  # You want to clean trash ? " + name + "!");
-            GetInfos();
+            Console.Write("Yes or No !");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("  > ");
+
+            try
+            {
+                GetInfos();
+            }
+            catch
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR");
+            }
         }
 
         static void GetInfos() {
-            string[] fils = Directory.GetFiles(@"C:\Users\GauravPun\Desktop\TMP\");
-            string[] fols = Directory.GetDirectories(@"C:\Users\GauravPun\Desktop\TMP\");
 
-            for (int i = 0; i < fils.Length; i++) {
-                Thread.Sleep(200);
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(fils[i]);
+            if (Directory.GetFiles(AppContext.BaseDirectory + "LocSettings") != null)
+            {
+                Console.WriteLine("Found!");
+                perTempLoc = File.ReadAllText(AppContext.BaseDirectory + @"LocSettings\loc1.wow");
+                tempLoc = File.ReadAllText(AppContext.BaseDirectory + @"LocSettings\loc2.wow");
+                prefetchLoc = File.ReadAllText(AppContext.BaseDirectory + @"LocSettings\loc3.wow");
+
+                perTempFils = Directory.GetFiles(perTempLoc);
+                perTempFols = Directory.GetDirectories(perTempLoc);
+
+                tempFils = Directory.GetFiles(tempLoc);
+                tempFols = Directory.GetDirectories(tempLoc);
+
+                preFils = Directory.GetFiles(perTempLoc);
+                preFols = Directory.GetDirectories(perTempLoc);
+
+                Console.WriteLine(perTempLoc);
+            }
+            else {
+                Console.WriteLine("NotFound!");
+                perTempFils = Directory.GetFiles(@"C:\Users\GauravPun\Desktop\tmp\");
+                perTempFols = Directory.GetDirectories(@"C:\Users\GauravPun\Desktop\tmp\");
+
+                tempFils = Directory.GetFiles(@"C:\Users\GauravPun\Desktop\tmp2\");
+                tempFols = Directory.GetDirectories(@"C:\Users\GauravPun\Desktop\tmp2\");
+
+                preFils = Directory.GetFiles(@"C:\Users\GauravPun\Desktop\tmp3\");
+                preFols = Directory.GetDirectories(@"C:\Users\GauravPun\Desktop\tmp3\");
             }
 
-            for (int i = 0; i < fols.Length; i++) {
-                Thread.Sleep(200);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine(fols[i]);
+
+            bool errorFound = false;
+
+            //Search for
+            string[] S = new string[] {
+                AppContext.BaseDirectory + @"LocSettings\loc1.wow",
+                AppContext.BaseDirectory + @"LocSettings\loc2.wow",
+                AppContext.BaseDirectory + @"LocSettings\loc3.wow"
+            };
+
+            try {
+                string[] SSame = Directory.GetFiles(AppContext.BaseDirectory + "LocSettings");
+
+                for (int i = 0; i < S.Length; i++)
+                {
+                    try
+                    {
+                        if (SSame[i] == S[i]){ } // Scan the files
+                    }
+                    catch
+                    {
+                        errorFound = true;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR !");
+
+                        Console.WriteLine("NotFound : " + "loc" + (i + 1) + ".wow");
+                        break;
+                    }
+                }
+            }
+            catch {
+                errorFound = true;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error....!");
+                Console.WriteLine("(*_*)!");
             }
 
-            Percent = (float)(fils.Length + fols.Length);
-            float A = 100 / Percent;
-
-            Console.WriteLine(A);
-
-            Console.ReadLine();
-            Delete(fils, fols);
+            if (Console.ReadLine().ToUpper() == "YES" && !errorFound)
+            {
+                Console.Clear();
+                Delete1(perTempFils, perTempFols);
+            }
         }
 
-        static void Delete(string[] filesLoc, string[] foldersLoc) {
+        static void Delete1(string[] filesLoc, string[] foldersLoc) {
+
+            Console.CursorVisible = false;
+
             for (int i = 0; i < filesLoc.Length; i++) {
                 try
                 {
+                    Thread.Sleep(50);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     //File.Delete(filesLoc[i]);
+                    Console.WriteLine(filesLoc[i]);
                 }
                 catch {
-
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-_-");
                 }
             }
+
+            for (int i = 0; i < foldersLoc.Length; i++)
+            {
+                try
+                {
+                    Thread.Sleep(50);
+                    //Directory.Delete(foldersLoc[i], true);
+                    Console.WriteLine(foldersLoc[i]);
+                }
+                catch {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-_-");
+                }
+            }
+
+            Delete2(tempFils, tempFols);
+        }
+
+        static void Delete2(string[] filesLoc, string[] foldersLoc)
+        {
+
+            Console.CursorVisible = false;
 
             for (int i = 0; i < filesLoc.Length; i++)
             {
                 try
                 {
-
+                    Thread.Sleep(50);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    //File.Delete(filesLoc[i]);
+                    Console.WriteLine(filesLoc[i]);
                 }
-                catch {
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-_-");
                 }
             }
 
-            Console.Clear();
+            for (int i = 0; i < foldersLoc.Length; i++)
+            {
+                try
+                {
+                    Thread.Sleep(50);
+                    //Directory.Delete(foldersLoc[i], true);
+                    Console.WriteLine(foldersLoc[i]);
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-_-");
+                }
+            }
+
+            Delete3(preFils, preFols);
+        }
+
+        static void Delete3(string[] filesLoc, string[] foldersLoc)
+        {
+
+            Console.CursorVisible = false;
+
+            for (int i = 0; i < filesLoc.Length; i++)
+            {
+                try
+                {
+                    Thread.Sleep(50);
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    //File.Delete(filesLoc[i]);
+                    Console.WriteLine(filesLoc[i]);
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-_-");
+                }
+            }
+
+            for (int i = 0; i < foldersLoc.Length; i++)
+            {
+                try
+                {
+                    Thread.Sleep(50);
+                    //Directory.Delete(foldersLoc[i], true);
+                    Console.WriteLine(foldersLoc[i]);
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-_-");
+                }
+            }
+
+            //Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Done!");
+            Console.WriteLine("Compelete!");
             Console.ReadLine();
         }
     }
